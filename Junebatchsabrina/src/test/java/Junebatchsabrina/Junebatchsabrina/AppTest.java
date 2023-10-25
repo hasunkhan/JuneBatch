d597202a721334c8cf1ef609b5fcfe326cd74be4
SabrSultana <sabsultana80@gmail.com>
2023-10-24 22:18:26 -0500

    First commit

---
+++ Junebatchsabrina/src/test/java/Junebatchsabrina/Junebatchsabrina/AppTest.java
@@ -0,0 +1,21 @@
+package Junebatchsabrina.Junebatchsabrina;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+    	System.out.println("I am sabrina");
+        assertTrue( true );
+    }
+}
