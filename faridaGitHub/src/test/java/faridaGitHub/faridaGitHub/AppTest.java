1df92f2927f67ee5906614cd117ecb56088476a7
atik0 <atik0@DESKTOP-4I8M57L>
2023-10-25 20:14:03 -0500

    First commit

---
+++ faridaGitHub/src/test/java/faridaGitHub/faridaGitHub/AppTest.java
@@ -0,0 +1,20 @@
+package faridaGitHub.faridaGitHub;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {System.out.println("I am Farida");
+        assertTrue( true );
+    }
+}
