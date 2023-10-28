911e50c90b86524294ebd6a0ea09b53b67f46f31
SabrSultana <sabsultana80@gmail.com>
2023-10-27 19:45:18 -0500

    First Commit and push in new folder

---
+++ Junebatchsabrina1/src/test/java/Junebatchsabrina1/Junebatchsabrina1/AppTest.java
@@ -0,0 +1,21 @@
+package Junebatchsabrina1.Junebatchsabrina1;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue() {
+    System.out.println("I am Sabrina sultana, I live in Tenessee");
+    
+        assertTrue( true );
+    }
+}
